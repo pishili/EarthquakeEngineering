82ce1cf4381b28c7b1b7616e03217aef6479d39b
Ladan <ladanks@gmail.com>
2018-09-26 13:38:10 -0700

    Add Tests for ToDoTask and ToDoList

--- personalproject/src/test/TestToDoList.java
+++ personalproject/src/test/TestToDoList.java
@@ -1,10 +1,26 @@
 package test;
 
+import model.ToDoTask;
+import model.ToDoList;
+
 public class TestToDoList {
+    private ToDoTask t1;
+    private ToDoTask t2;
+    private ToDoTask t3;
+    private ToDoTask t4;
+
+    private ToDoList myTasks;
 
     public TestToDoList() {
+        t1 = new ToDoTask("cs221", "not started", "super improtant");
+        t2 = new ToDoTask("213", "not started", "important");
+        t3 = new ToDoTask("121", "finished", "super important");
+        t4 = new ToDoTask("210", "In Progress", "Super important");
 
+        myTasks = new ToDoList("MY TASKS");
+        myTasks.addTask(t1);
+        myTasks.addTask(t2);
+        myTasks.addTask(t3);
+        myTasks.addTask(t4);
     }
-
-    @
 }
---
+++ personalproject/src/test/TestToDoTask.java
@@ -0,0 +1,24 @@
+package test;
+
+import model.ToDoTask;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestToDoTask {
+    private ToDoTask task1;
+
+
+    @BeforeEach
+    public void runBefore() {
+        task1 = new ToDoTask("review meeting", "not finished", "very important");
+    }
+
+    @Test
+    public void testConstructor(){
+        assertEquals("review meeting", task1.getTitle());
+        assertEquals("not finished", task1.getStatus());
+        assertEquals("very important", task1.getPriority());
+    }
+}
